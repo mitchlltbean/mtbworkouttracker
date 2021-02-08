9fef9e6fd07618692dd4406e5552c69d268f4895
Mitchell Bean <mitchtroybean@gmail.com>
2021-02-07 21:25:19 -0800

    basic file structure with server info

---
+++ server.js
@@ -0,0 +1,66 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require("express");
+// const session = require("express-session");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+// require("dotenv").config();
+// Requiring our models for syncing
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 2,
+//     },
+//   })
+// );
+
+// Static directory
+app.use(express.static("public"));
+
+// =============================================================
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// =============================================================
+
+// Routes -- ROUTES MUST COME AFTER MIDDLEWARE AND HANDLEBARS
+// =============================================================
+// const userRoutes = require("./controllers/userController");
+// app.use(userRoutes);
+
+// const frontEndRoutes = require("./controllers/frontEndController");
+// app.use(frontEndRoutes);
+
+// const followingRoutes = require("./controllers/followingController");
+// app.use(followingRoutes);
+
+// const searchRoutes = require("./controllers/searchController");
+// app.use(searchRoutes);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
